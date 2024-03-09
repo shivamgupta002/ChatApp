25bff4eaaf735aedbe50accd112df656e0d11ed9
shivamgupta002 <shivamgupta992909@gmail.com>
2024-03-09 15:26:36 +0530

    New Group added

--- client/src/components/specific/NewGroup.jsx
+++ client/src/components/specific/NewGroup.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useInputValidation } from "6pp";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { sampleUsers } from "../../constants/sampleData";
+import UserItem from "../shared/UserItem";
 
 const NewGroup = () => {
-  return (
-    <div>NewGroup</div>
-  )
-}
+  const groupName = useInputValidation("");
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectMember, setSelectedMember] = useState([]);
+  const selectMemberHandler = (id) => {
+    setSelectedMember((prev) =>
+      prev.includes(id)
+        ? prev.filter((currElement) => currElement !== id)
+        : [...prev, id]
+    );
+  };
+  // console.log(selectMember);
+  const submitHandler = () => {};
+  const closeHandler = () => {};
 
-export default NewGroup
+  return (
+    <Dialog open>
+      <Stack p={{ sx: "1rem", sm: "3rem" }} width={"25rem"} spacing={"0.5rem"}>
+        <DialogTitle textAlign={"center"} variant="h4">
+          New Group
+        </DialogTitle>
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+        <Typography variant="body1">Members</Typography>
+        <Stack>
+          {members.map((i) => (
+            <UserItem
+              user={i}
+              key={i._id}
+              handler={selectMemberHandler}
+              isAdded={selectMember.includes(i._id)}
+            />
+          ))}
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <Button
+            variant="text"
+            color="error"
+            size="large"
+            onClick={closeHandler}
+          >
+            Cancel
+          </Button>
+          <Button variant="contained" size="large" onClick={submitHandler}>
+            Create
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default NewGroup;
